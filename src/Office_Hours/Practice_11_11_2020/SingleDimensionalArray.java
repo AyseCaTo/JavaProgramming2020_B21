0fa0439572f3b0f6b05dc6b74703a98d928ad722
AyseCaTo <ayshetyglu@gmail.com>
2020-11-12 20:51:07 -0500

    adding Scanner

--- src/Office_Hours/Practice_11_11_2020/SingleDimensionalArray.java
+++ src/Office_Hours/Practice_11_11_2020/SingleDimensionalArray.java
@@ -1,10 +1,13 @@
 package Office_Hours.Practice_11_11_2020;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class SingleDimensionalArray {
 
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
 
         int[] scores = new int[1];
 
