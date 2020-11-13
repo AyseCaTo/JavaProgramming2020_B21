c617375b58f420eed8dcf70a2916d999df248454
AyseCaTo <ayshetyglu@gmail.com>
2020-11-12 20:18:14 -0500

    initial commit

---
+++ src/Office_Hours/Practice_11_11_2020/SingleDimensionalArray.java
@@ -0,0 +1,12 @@
+package Office_Hours.Practice_11_11_2020;
+
+public class SingleDimensionalArray {
+
+    public static void main(String[] args) {
+
+        int[] scores = new int[1];
+
+        scores[0] = 10;
+        scores [1] = 20;
+    }
+}
