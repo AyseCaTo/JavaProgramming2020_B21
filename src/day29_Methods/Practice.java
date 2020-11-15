e5c2fa0be79deb81d87f3812d79f594f2dc4aa2e
AyseCaTo <ayshetyglu@gmail.com>
2020-11-14 22:52:50 -0500

    printing hello n times

---
+++ src/day29_Methods/Practice.java
@@ -0,0 +1,20 @@
+package day29_Methods;
+
+public class Practice {
+
+    public static  void printHellonTimes(int n){
+        for (int i = 1; i<=n; i++){
+            System.out.println("hello");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        printHellonTimes(7);
+        System.out.println("Ayse");
+        printHellonTimes(9);
+        System.out.println("Harvard University");
+        printHellonTimes(11);
+        System.out.println("Apple Company");
+    }
+}
