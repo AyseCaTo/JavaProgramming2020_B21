28a814db2c946f2ccf7bca33bde13eec2472fcad
AyseCaTo <ayshetyglu@gmail.com>
2020-11-14 23:37:49 -0500

    created new method

--- src/day29_Methods/EligibleToVote.java
+++ src/day29_Methods/EligibleToVote.java
@@ -2,5 +2,19 @@ package day29_Methods;
 
 public class EligibleToVote {
 
+    public static void main(String[] args) {
 
+    }
+
+    public static void eligible(int age, String citizenship){
+        if (citizenship.equalsIgnoreCase("USA")){
+            if (age>=18){
+                System.out.println("Congratulations! You are eligible to vote!");
+            }else{
+                System.err.println("You must grown up!");
+            }
+        }else{
+            System.err.println("You must be a US citizen!");
+        }
+    }
 }
