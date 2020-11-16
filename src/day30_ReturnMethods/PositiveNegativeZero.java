07e91cb5b62ab36a9ce6e81ea2e4d68d9065d8a0
AyseCaTo <ayshetyglu@gmail.com>
2020-11-16 10:03:41 -0500

    finding positive negative zero

--- src/day30_ReturnMethods/PositiveNegativeZero.java
+++ src/day30_ReturnMethods/PositiveNegativeZero.java
@@ -1,4 +1,21 @@
 package day30_ReturnMethods;
 
 public class PositiveNegativeZero {
+
+    public static void main(String[] args) {
+        positiveNegativeZero(-20);
+        positiveNegativeZero(0);
+        positiveNegativeZero(78);
+
+    }
+
+    public static void positiveNegativeZero(int number){
+        if (number>0){
+            System.out.println(number + " is positive");
+        }else if (number<0){
+            System.out.println(number + " is negative");
+        }else{
+            System.out.println(number + " is zero");
+        }
+    }
 }
